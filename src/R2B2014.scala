e33ab8afe573cd9ba9992de87b60f69fe2131953
jlee <cheesespy@gmail.com>
2015-04-02 21:41:58 -0700

    R2B2014

--- src/R2B2014.scala
+++ src/R2B2014.scala
@@ -5,13 +5,58 @@ import java.io.IOException
  */
 class R2B2014(fileName: String) extends CodeJam(fileName) {
   def solve(): Unit = {
+    br.readLine
+    val row: Array[Int] = br.readLine.split("\\s+").map(_.toInt)
+    var start = 0
+    var end = row.length - 1
+
+    var move = 0
+    while (start < end) {
+      var mi = min(row, start, end)
+
+      if (mi - start < end - mi) {
+        while (mi > start) {
+          val temp = row(mi - 1)
+          row(mi - 1) = row(mi)
+          row(mi) = temp
+          move += 1
+          mi-=1
+        }
+        start += 1
+      } else {
+        while (mi < end) {
+          val temp = row(mi + 1)
+          row(mi + 1) = row(mi)
+          row(mi) = temp
+          move += 1
+          mi+=1
+        }
+        end -= 1
+      }
+    }
+
+    printResult(move.toString)
+
+    def min(arr: Array[Int], start: Int, end: Int): Int = {
+      var min = Int.MaxValue
+      var index = 0
+      for (i <- start to end) {
+        if (arr(i) < min) {
+          min = arr(i)
+          index = i
+        }
+      }
+      index
+    }
 
   }
 
   def init(): Unit = {
 
   }
+}
 
+object R2B2014 {
   def main(args: Array[String]) {
     var fileName: String = "A-"
     if (args(0).compareTo("small") == 0) {
@@ -34,7 +79,7 @@ class R2B2014(fileName: String) extends CodeJam(fileName) {
       fileName += ".in.txt"
     }
     try {
-      val codeJam: R1B2013b = new R1B2013b(fileName)
+      val codeJam: R2B2014 = new R2B2014(fileName)
       codeJam.run
     }
     catch {
