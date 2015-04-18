8c93374ac13557b84b4fca0ae3a219e9667c9a02
jlee <cheesespy@gmail.com>
2015-04-18 08:49:41 -0700

    codejam

--- src/R1B2015.scala
+++ src/R1B2015.scala
@@ -1,146 +1,57 @@
 import scala.annotation.tailrec
 
+
 /**
  * Created by essis on 15. 3. 15..
  */
 class R1B2015(fileName: String) extends CodeJam(fileName) {
 
-  def GCD(a: Long, b: Long): Long = {
-    var tmp = 0L
-    var bb = b
-    var aa = a
-    while(bb > 0) {
-      tmp = aa
-      aa = bb
-      bb = tmp % bb
-    }
-
-    aa
+  def GCD(a: BigInt, b: BigInt): BigInt = {
+    if (b == BigInt(0)) a
+    else GCD(b, a % b)
   }
 
-  private def bsearch2(a: Long, b: Long, target: Long, ar: Array[Long]): Int = {
-    if (a == b) {
-      val count = ar.fold(0L)((x,y) => if (a % y == 0 ) x + a / y else  x + a / y + 1)
-      val t = target - count
-      var c = 0
-      for (i <- ar.indices) {
-        if (a % ar(i) == 0) {
-          c += 1
-          if (c==t)
-            return i
-        }
-      }
-      throw new Exception("error3")
-    }
+  def LCM(a: BigInt, b: BigInt): BigInt = {
+    (a * b) / GCD(a,b)
+  }
 
+  @tailrec
+  private def bsearch2(a: BigInt, b: BigInt, target: BigInt, ar: Array[BigInt]): Int = {
 
     val center = (a + b) / 2
-    val count = ar.fold(0L)((x,y) => x + center / y + 1)
+    val count = ar.fold(BigInt(0))((x,y) => if (center % y == BigInt(0) ) x + center / y  else  x + center / y + 1)
+    var c = 0
+    for (i <- ar.indices) {
+      if (center % ar(i) == BigInt(0)) {
+        c += 1
+        if (count + c == target) {
+          return i
 
-//    println(s"a $a, b $b, center $center, count $count, target $target")
-    if (count == target) {
-      var sol = 0
-      for (i <- ar.indices) {
-        if (center % ar(i) == 0) {
-          sol = i
         }
       }
-      sol
-    } else if (count > target) {
+    }
+
+   if (count + c >= target) {
       bsearch2(a, center, target, ar)
     } else {
       bsearch2(center+1, b, target, ar)
     }
   }
 
-  /*private def bsearch(a: Long, b: Long, target: Long, ar: Array[Long]): Int = {
-    if (a == b) {
-      val count = ar.fold(0L)((x,y) => if (a % y == 0 ) x + a / y else  x + a / y + 1)
-      val t = target - count
-      var c = 0
-      for (i <- ar.indices) {
-        if (a % ar(i) == 0) {
-          c += 1
-          if (c==t)
-            return i
-        }
-      }
-      throw new Exception("error3")
-    }
-
-    if (a == 0 && a + 1 == b) {
-      var c = 0
-      for (i <- ar.indices) {
-        if (a % ar(i) == 0) {
-          c += 1
-          if (c==target)
-            return i
-        }
-      }
-      throw new Exception("error4")
-
-    }
-
-    val center = (a + b) / 2
-    val count = ar.fold(0L)((x,y) => x + center / y + 1)
-    val countd = ar.fold(0L)((x,y) => x + (center - 1) / y + 1)
-
-    println(s"a $a, b $b, center $center, count $count, countd $countd, target $target")
-    if (count == target) {
-      var sol = 0
-      var min = Long.MaxValue
-      for (i <- ar.indices) {
-        if (center % ar(i) <= min) {
-          min = center % ar(i)
-          sol = i
-        }
-      }
-      sol
-    } else if (countd == target) {
-      var sol = 0
-      var min = Long.MaxValue
-      for (i <- ar.indices) {
-        if ((center-1) % ar(i) <= min) {
-          min = (center-1) % ar(i)
-          sol = i
-        }
-      }
-      sol
-    } else if( countd < target && count > target) {
-      val t = target - countd
-      var c = 0
-      for (i <- ar.indices) {
-        if (center % ar(i) == 0) {
-          c += 1
-          if (c==t)
-            return i
-        }
-      }
-      throw new Exception("error2")
-    } else if (count > target) {
-      bsearch(a, center, target, ar)
-    } else {
-      bsearch(center, b, target, ar)
-    }
-
-  }*/
-
 
   def solve(): Unit = {
-    val row: Array[Long] = br.readLine.split("\\s+").map(_.toLong)
+    val row: Array[BigInt] = br.readLine.split("\\s+").map(BigInt(_))
     val B = row(0).toInt
     val N = row(1)
-    val ar: Array[Long] = br.readLine.split("\\s+").map(_.toLong)
-    val gcd = ar.reduce(GCD)
-    val lcm = ar.reduce((a, b) => (a * b) / gcd)
-    val batch = ar.fold(0L)((a,b) => a + (lcm / b))
-    println(s"lcm $lcm, batch $batch")
-    if (N % batch == 0) {
-      var m = Long.MaxValue
+    val ar: Array[BigInt] = br.readLine.split("\\s+").map(BigInt(_))
+    val lcm = ar.reduce(LCM)
+    val batch = ar.fold(BigInt(0))((a,b) => a + (lcm / b))
+    if (N % batch == BigInt(0)) {
+      var m = lcm
       var sol = 0
       for (i <- ar.indices) {
-        if ((batch - 1)% ar(i) <= m) {
-          m = (batch - 1) % ar(i)
+        if ((lcm - 1)% ar(i) <= m) {
+          m = (lcm - 1) % ar(i)
           sol = i
         }
       }
